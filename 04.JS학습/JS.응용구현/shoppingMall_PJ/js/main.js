c044c979f61ecb27f67e26a0d6590b4daaf8efed
chan1122 <zoro3958@naver.com>
2022-10-27 12:15:55 +0900

    나는 모른다!!

--- 04.JS학습/JS.응용구현/shoppingMall_PJ/js/main.js
+++ 04.JS학습/JS.응용구현/shoppingMall_PJ/js/main.js
@@ -31,125 +31,129 @@ window.addEventListener("DOMContentLoaded", loadFn);
             li에 클래스 "on"주기(나머진 빼기->초기화!)
 
 *****************************************************/
+
 /****************************************** 
     함수명: loadFn
     기능: 로딩 후 버튼 이벤트 및 기능구현
-    ******************************************/
-    let prot = 0;
-    // 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
-    let pnum = 100;
+******************************************/
 function loadFn() {
-  // 1. 호출확인
-  console.log("로딩완료!");
-  // 광클 금지 함수!!!!!!
-  // 광클 금지 상태변수
+    // 1. 호출확인
+    console.log("로딩완료!");
 
-  if(prot) return; //나가!
-  prot = 1;
-  setTimeout(() => 
-    prot=0,800);
+    // 광클금지 상태변수
+    let prot = 0; // 0-허용, 1-불허용
 
-  // 2 . 대상 선정 :
-  // 2 - 1 . 이벤트 대상 : .abtn
-  const abtn = document.querySelectorAll(".abtn");
-  // 2 - 3 . 변경 대상 : #slide
-  const slide = document.querySelector("#slide");
-  abtn[1].onclick = () => {
-    // 1 1 1 1 1 1 1 1 1 1 1 11
-    // pnum += 100;
+    // 2. 대상선정
+    // 2-1. 이벤트대상: .abtn
+    const abtn = document.querySelectorAll(".abtn");
+    // 2-2. 변경대상: #slide
+    const slide = document.querySelector("#slide");
+    // 2-3. 블릿변경대상: .indic li
+    const indic = document.querySelectorAll(".indic li");
 
-    slide.style.left = `-${pnum}%`;
-    slide.style.transition = ".8s";
+    // 3. 이벤트 설정하기 및 기능구현
+    // 3-1. 오른쪽 버튼 클릭시
+    abtn[1].onclick = () => {
+        // 광클금지 ////////////
+        if (prot) return; // 나가!
+        prot = 1; // 잠금!
+        setTimeout(() => (prot = 0), 800);
+        ////////////////////////////
 
-    // 이거 주석 풀자잉!!!  ! ! ! ! ! ! !! ! !
-    // 0.8 초후 맨앞 li 잘라서 맨뒤로 이동
-    setTimeout(() => {
-      slide.appendChild(slide.querySelectorAll("li")[0]);
-      slide.style.left = "0";
-      slide.style.transition = "none";
-    }, 800);
+        slide.style.left = "-100%";
+        slide.style.transition = ".8s ease-in-out";
+        // 0.8초후 맨앞li 잘라서 맨뒤로 이동!
+        setTimeout(() => {
+            // 1. 맨앞li 잘라서 맨뒤로 이동!
+            slide.appendChild(slide.querySelectorAll("li")[0]);
+            // 2. left값 0으로 초기화!
+            slide.style.left = "0";
+            /* 3. 트랜지션 없애기 */
+            slide.style.transition = "none";
+        }, 800);
 
-    //불릿 함수 호출!
-    // -> 오른쪽 버튼은 
-    chgIndic(1)
-  }; ////////////////// 클릭 //////////////////
+        // 블릿변경함수 호출!
+        // -> 오른쪽버튼은 두번째 슬라이드가 주인공!
+        chgIndic(1);
 
+        // 자동넘김지우기 함수 호출!
+        clearAuto();
+    }; //////////// click ////////////////
 
-  // 3 - 2 . 왼쪽 버튼 클릭시 : 외쪽버튼 abtn변수 0번째
-  abtn[0].onclick = function(){
-    if(prot) return; //나가!
-    prot = 1;
-    setTimeout(() => 
-      prot=0,800);
-    //  1 . 맨뒤요소를 잘라서 맨앞으로 이동한다!
-    // 대상: slide변수 -> ul#slide
-    // 사용 메서드: insertBefore(넣을놈,넣을놈 전놈)
-    let cli = slide.querySelectorAll("li");
-    let ssllidd = slide.insertBefore(cli[cli.length-1],cli[0]);
-    //  2 . 왼쪽 바깥에 -100% left값 주기!
-    slide.style.left = "-100%";
-    // 첫번째 실행후 트랜지션 없애기
-    slide.style.transition = "";
-    
-    //2 번과 3번 코드 사이에 시차 필요 
-    setTimeout(() => {
-      
-      // 3 . left값을 0으로 트랜지션 애니메이션하기
-      slide.style.left = "0";
-      slide.style.transition = ".8s";
-    }, 0);
+    // 3-2. 왼쪽버튼 클릭시 : 왼쪽버튼 abtn변수 0번째
+    abtn[0].onclick = () => {
+        // 광클금지 ////////////
+        if (prot) return; // 나가!
+        prot = 1; // 잠금!
+        setTimeout(() => (prot = 0), 800);
+        ////////////////////////////
 
-    // 불릿 변경 함수 호출 !
-    // -> 왼쪽 버튼은 첫번째 슬라이드가 주인공!
-    chgIndic();
+        // 1. 맨뒤요소를 잘라서 맨앞으로 이동한다!
+        // 대상: slide변수 -> ul#slide
+        // 사용메서드: insertBefore(넣을놈,넣을놈전놈)
+        // 현재 li자식요소 수집하기!
+        let cli = slide.querySelectorAll("li");
+        slide.insertBefore(cli[cli.length - 1], cli[0]);
 
-  };
+        // 2. 왼쪽 바깥에 -100% left값 주기!
+        slide.style.left = "-100%";
+        // 첫번째 실행후 생긴 트랜지션 없애기!
+        slide.style.transition = "none";
 
-  // 4 . 슬라이드가 이동 되므로 순서를 알수 있는 
-  // 고유순번용 속성을 만들어서 넣어준다!
-  // w3c 에서 허용한 사용자 지정 송성명은 반드시 data-로
-  // 시작해야하므로 우리는 "data-seq" 라는 이름을 쓰자!
-  // 대상: #slide li -> 여기에 속성을 넣는다
-  // 
-  // 사용 메서드 : forEach((요소,순번)=>{코드})
-  slide.querySelectorAll("li")
-  .forEach((ele,idx)=>{
+        // 2번과 3번 코드 사이에 시차필요!!!
+        // setTimeout()사용!
+        // -> 시간을 0으로 해도 코드실행구역을 분리하므로
+        // 코드가 별도로 실행된다!(순서를 지킴!)
+        setTimeout(() => {
+            // 3. left값을 0으로 트랜지션 애니메이션하기
+            slide.style.left = "0";
+            slide.style.transition = ".8s ease-in-out";
+        }, 0);
 
-    ele.setAttribute("data-seq",idx);
-  });// for Each
+        // 블릿변경함수 호출!
+        // -> 왼쪽버튼은 첫번째 슬라이드가 주인공!
+        chgIndic(0);
 
-  // 5 . 불릿 변경 함수!
-  // 버튼을 클릭할때 블릿을 해당 순번의 슬라이드와 같은 
-  //순번의 블릿의 li에 클래스"on을 넣고
-  // 나머지는 클래스를 제거함!
-  // chgIndic -> change Indicator (변경하라! 표시자를!)
-  const chgIndic = idx => { // idx - 대상 슬라이드 순번
+        // 자동넘김지우기 함수 호출!
+        clearAuto();
+    }; ///////////// click //////////////
 
-    // 1 . 현제 슬라이드 순번 알아오기
-    // cseq -> current sequence number(현제순번)
-    let cseq = slide.querySelectorAll("li")[0] // 해당 li 순번
-    .getAttribute("data-seq"); // 그 li에 "data-seq"속성값
-    // getAttribute(속성명) -> 속성값 읽어오는 JS 내장함수
+    // 4. 슬라이드가 이동되므로 순서를 알 수 있는
+    // 고유순번용 속성을 만들어서 넣어준다!
+    // w3c에서 허용한 사용자 지정속성명은 반드시 data- 로
+    // 시작해야하므로 우리는 "data-seq"라는 이름을 쓰자!
+    // 대상: #slide li -> 여기에 속성을 넣는다!
+    // 사용메서드 : forEach((요소,순번)=>{코드})
+    // 사용메서드 : setAttribute(속성명,값)
+    slide.querySelectorAll("li").forEach((ele, idx) => {
+        ele.setAttribute("data-seq", idx);
+    }); /////// forEach ////////////////
 
-    console.log(cseq)
-    // 2 . 슬라이드 li클래스 초기화
-    for(let x of indic) x.classList.remove("on");
+    // 5. 블릿변경함수
+    // 버튼을 클릭할때 블릿을 해당순번의 슬라이드와 같은
+    // 순번의 블릿의 li에 클래스"on"을 넣고
+    // 나머지는 클래스를 제거함
+    // chgIndic -> change Indicator (변경하라! 표시자를!)
+    const chgIndic = (idx) => {
+        // idx - 대상슬라이드 순번
+        // 1. 현재 슬라이드 순번 알아오기!
+        // cseq -> current sequence number(현재 순번)
+        let cseq = slide
+            .querySelectorAll("li")
+            [idx] // 대상 li
+            .getAttribute("data-seq"); // "data-seq"속성값
+        // getAttribute(속성명) -> 속성값 읽어오는 JS내장함수
 
-    // 2 . 슬라이드 순번과 동일한 블릿 li 클래스 "on"넣기
-    indic[cseq].classList.add("on");
+        console.log("순번:", cseq);
 
-    
+        // 2. 슬라이드 li 클래스 초기화!
+        for (let x of indic) x.classList.remove("on");
 
-  }; ////////// chgIndic 함수 //////////
+        // 2. 슬라이드 순번과 동일한 블릿li 클래스 "on"넣기
+        indic[cseq].classList.add("on");
+    }; ////////////// chgIndic 함수 ////////////////
 
-
-
-
-
-
-
-
- /***************************************** 
+    /***************************************** 
         자동넘기기 기능구현
     *****************************************/
     // 인터발용변수
@@ -167,8 +171,22 @@ function loadFn() {
     *******************************/
     function slideAuto() {
         autoI = setInterval(() => {
-          slide.style.left = "-100%";
-          slide.style.transition = ".8s";
+            // 오른쪽버튼 클릭시 이동코드와 동일함!!!
+            slide.style.left = "-100%";
+            slide.style.transition = ".8s ease-in-out";
+            // 0.8초후 맨앞li 잘라서 맨뒤로 이동!
+            setTimeout(() => {
+                // 1. 맨앞li 잘라서 맨뒤로 이동!
+                slide.appendChild(slide.querySelectorAll("li")[0]);
+                // 2. left값 0으로 초기화!
+                slide.style.left = "0";
+                /* 3. 트랜지션 없애기 */
+                slide.style.transition = "none";
+            }, 800);
+
+            // 블릿변경함수 호출!
+            // -> 오른쪽버튼은 두번째 슬라이드가 주인공!
+            chgIndic(1);
         }, 2000);
     } //////// slideAuto 함수 //////////
 
@@ -188,7 +206,5 @@ function loadFn() {
         // 3. 일정시간 후 다시 인터발 호출!
         autoT = setTimeout(slideAuto, 5000);
     } /////// clearAuto함수 ////////
-
-
 } //////////////// loadFn 함수 ///////////////
 /////////////////////////////////////////////
