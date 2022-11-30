a505f566ecfff6ece874bf89d1728455c1ed2302
chan1122 <zoro3958@naver.com>
2022-11-30 12:43:20 +0900

    aa

--- 06.React학습/react-app/my-react-app/src/DC/Layout.js
+++ 06.React학습/react-app/my-react-app/src/DC/Layout.js
@@ -1,59 +1,72 @@
-import React from "react";
-import { Outlet ,Link } from "react-router-dom";
-import "./Layout.css"
+import {Outlet, Link} from "react-router-dom";
 import Logo from "./Logo";
+import "./Layout.css";
 /* 
-  [ 리액트 스타일링 기법 3가지 ]
-  1. 일반 css파이을 컴포넌트 js파일에 import하여 사용
-  에) import "css경로";
+    [ 리엑트 스타일링 기법 3가지 ]
+    1. 일반 CSS파일을 컴포넌트 JS파일에 import하여 사용
+    예) import "CSS경로";
 
+    2. 객체를 만들어서 적용하는 방법
+    예)
+    const mystyle = {color:"red",fontSize:"30px"};
+    <h1 style={mystyle}>하하하</h1>
 
-  2. 객체를 만들어서 적용하는 방법
-  에) 
-  const mystyle = {color:"red",fontSize:"30px"}
-  <h1 styel={}> 안녕 나는 h1이야 </h1>
-  3. 직접 중괄호 표현식에 중괄호를 하나 더 하여
-  표현
-  에) 
-
-  <h1 styel={}> </h1>
+    3. 직접 중괄호 표현식에 중괄호를 하나 더 하여 표현
+    -> 중괄호 하나는 표현식, 안에 중괄호는 객체(CSS용)
+    예)
+    <h1 style={{color:"red"}}>하하하</h1>
 */
-const mystyle = {color:"red",fontSize:"30px"}
+
 const Layout = () => {
-  return (
-    <React.Fragment>
-      {/* 네비게이션 파트 */}
-      <nav className="gnb">
-        <ul>
-          <li><Logo /></li>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/ct">CHARACTERS</Link>
-          </li>
-          <li>
-            <Link to="/co">COMICS</Link>
-          </li>
-          <li>
-            <Link to="/gm">GAMES</Link>
-          </li>
-          <li>
-            <Link to="/mv">MOVIES</Link>
-          </li>
-          <li>
-            <Link to="/vd">VIDEO</Link>
-          </li>
-          <li>
-            <Link to="/nw">NEWS</Link>
-          </li>
-        </ul>
-      </nav>
-      {/* 출력파트 : 각페이지가 여기에 표시됨 */}
-      <Outlet />
-    </React.Fragment>
-  );
+    return(
+        <>
+        {/* 1.상단영역 */}
+        <header className="top">
+        {/* 네비게이션 파트 */}
+            <nav className="gnb">
+                <ul>
+                    <li>
+                        <Logo />
+                    </li>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/ct">CHARACTERS</Link>
+                    </li>
+                    <li>
+                        <Link to="/co">COMICS</Link>
+                    </li>
+                    <li>
+                        <Link to="/mv">MOVIES</Link>
+                    </li>
+                    <li>
+                        <Link to="/gm">GAMES</Link>
+                    </li>
+                    <li>
+                        <Link to="/vd">VIDEO</Link>
+                    </li>
+                    <li>
+                        <Link to="/nw">NEWS</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        {/* 2.메인영역 */}
+        <main className="cont">         
+            {/* 출력파트 : 각 페이지가 표시됨 */}
+            <Outlet />
+        </main>
+        {/* 3.하단영역 */}
+        <footer className="info">
+            All Site Content © &amp; TM DC, unless otherwise noted here.
+            <br /> 
+            All rights reserved. 
+        </footer>
+           
+        </>
+    );
 };
 
-// Layout 내보내기
+// 내보내기 필수!
 export default Layout;
